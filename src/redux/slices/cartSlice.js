27cfa5f5a6a3e5a8a2f494cabae82f128cfff9ac
agustinbelisle <151087066+agustinbelisle@users.noreply.github.com>
2025-08-17 16:36:36 -0300

    react-vite

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL + "/api";
+
 
 // --- Async thunks ---
 
